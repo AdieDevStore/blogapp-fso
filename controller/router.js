b6bf9f1c63480730725c9beb27392ed65b1a731e
Adrian <60854914+AdieDevStore@users.noreply.github.com>
2024-04-07 16:50:27 +0200

    added async and more testing

--- controller/router.js
+++ controller/router.js
@@ -1,27 +1,53 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+// find all
+blogRouter.get('/', async (request, response, next) => {
+  const findAll = await Blog.find({})
+  response.json(findAll).status(200)
 })
 
-blogRouter.post('/', (request, response, next) => {
+// post onen
+blogRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
+
+  if (blog.likes != Number) {
+    blog.likes = 0
+  }
 
   if ( !request.body.title || !request.body.author ) {
     response.status(400).end()
     return
   }
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  await blog.save()
+  response.status(201).json(blog)
+})
+
+// find one
+blogRouter.get('/:id', async(request, response) => {
+  const id = request.params.id
+  const checkIfExists = await Blog.exists({_id: id})
+
+  if (!checkIfExists) {
+    response.status(404).end()
+  }
+
+  const findOne = await Blog.findById(id)
+  response.json(findOne).status(200)
+})
+
+// delete one
+blogRouter.delete('/delete/:id', async (request, response) => {
+  const id = request.params.id
+  const checkIfExists = await Blog.exists(({_id: id}))
+
+   if (!checkIfExists) {
+    response.status(404).end()
+  }
+
+  await Blog.deleteOne( {_id: id} )
+  response.status(204).end()
 })
 
 module.exports = blogRouter
--- test/backend.test.js
+++ test/backend.test.js
@@ -43,6 +43,66 @@ test('check if post requests are handles correctly', async () => {
   assert.strictEqual(notes.body.length, blogs.length + 1)
 })
 
+test('blog likes set to zero if type not equal to number', async () => {
+  const post = {
+    author: 'Adrian Viljoen', 
+    title: 'Importance of Testing Code', 
+    url: '', 
+  }
+
+  const request = await api
+  .post('/api/blogs')
+  .send(post)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(request.body.likes, 0)
+})
+
+test('blogs has missing information, return 400', async () => {
+  const post = {}
+
+  const request = await api
+  .post('/api/blogs')
+  .send(post)
+  .expect(400)
+
+  assert.strictEqual(request.statusCode, 400)
+})
+
+test('get one blog from list', async () => {
+  const id = '5a422a851b54a676234d17f7'
+
+  const singleBlog = await api
+  .get(`/api/blogs/${id}`)
+  
+  assert.strictEqual(singleBlog.body.title, 'React patterns')
+})
+
+test('delete one blog entry from DB', async () => {
+  const originalNotes = blogs.length
+  const id = '5a422a851b54a676234d17f7'
+
+  await api
+  .delete(`/api/blogs/delete/${id}`)
+  .expect(204)
+
+  const notesAfterDelete = await api
+  .get('/api/blogs')
+
+  assert.strictEqual(notesAfterDelete.body.length, originalNotes - 1)
+})
+
+test('attempt to delete blog that does not exist', async () => {
+  const fakeID = '5a422a851b54a676234d1300'
+
+  const response = await api 
+  .delete(`/api/blogs/delete/${fakeID}`)
+  .expect(404)
+
+  assert.strictEqual(response.statusCode, 404)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
