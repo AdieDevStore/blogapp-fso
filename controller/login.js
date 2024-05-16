21e50aade27e294b571d50c26c6e9deccc611117
Adrian <60854914+AdieDevStore@users.noreply.github.com>
2024-05-16 19:52:45 +0200

    cleaned up login again

--- controller/login.js
+++ controller/login.js
@@ -3,38 +3,46 @@ const knex = require('../utils/knex')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+  // now only returns one user 
+  const getUser = async (username) => {
+    const result = await knex.select().from('users').where({username: username}).then(result => result[0])
+    return result
+  }
+  
+  const checkPassword = async (password, fetchedUserPasswrd) => {
+    const result = await bcrypt.compare(password, fetchedUserPasswrd)
+    return result
+  }
+
+  const createToken = (username, id) => {
+    const token = {username: username, id: id}
+    return jwt.sign(token, process.env.SECRET)
+  }
+
 loginRouter.post('/', async (req, res) => {
   const {username, password} = req.body
 
   if (!username || !password) {
     return res.status(400).json({message: 'no user/password information'})
   }
-  
-  // does not return an array, just a single user
-  const getUser = async (username) => {
-    const result = await knex.select().from('users').where({username: username}).then(result => result[0])
-    return result
-  } 
-
+ 
   const user = await getUser(username)
 
   if (!user) {
-    return res.status(404).json({message: 'no user found'})
+    return res.status(400).json({message: 'no user exists'})
   }
 
-  const isSafe = await bcrypt.compare(password, user.password)
-  const usersToken = {
-    username: user.username,
-    id: user.id
-  }
-
-  const token = jwt.sign(usersToken, process.env.SECRET)
-
-  if (!isSafe) {
-    return res.status(401).json({message: 'passowrd incorrect'})
-    
-  } else {
+  try {
+    const match = await checkPassword(password, user.password_hash)
+    if (!match) {
+      res.status(401).json({message: 'passowrd incorrect'})
+    } else {
+      const token = createToken(user.username, user.id)
       return res.status(201).send({token, username: user.username}).end()
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({message: 'and error occured, check trace'})
   }
   
 })
